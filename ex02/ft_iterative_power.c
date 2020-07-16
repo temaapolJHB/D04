edb24cd6a9922ee1d036bc2bdadbe493f97dfdef
Tebatso Maapola <temaapol@c5r10s8.wethinkcode.co.za>
2020-07-16 14:01:09 +0200

    code

---
+++ ex02/ft_iterative_power.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: temaapol <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/15 14:34:45 by temaapol          #+#    #+#             */
+/*   Updated: 2020/07/16 11:05:01 by temaapol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+int ft_iterative_power(int nb, int power)
+{
+   int res;
+
+   res = 1;
+   if((nb == 0 && power != 0)
+     {
+	   return (0);
+	 }
+   if((power == 0) || ((nb < 0) && (power == )))
+     {
+        return (1);
+	 }
+
+   else
+	 {
+        while(power > 0)
+		{
+		  res = nb * result;
+		  power++;
+	    }
+		 return (res);
+	 }
+			  
+} 
