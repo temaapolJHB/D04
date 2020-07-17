db1deada971a30a4aaadca4d94378d6a5a045a12
Tebatso Maapola <temaapol@c5r10s8.wethinkcode.co.za>
2020-07-17 14:19:05 +0200

    code

--- ex02/ft_iterative_power.c
+++ ex02/ft_iterative_power.c
@@ -6,7 +6,7 @@
 /*   By: temaapol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/15 14:34:45 by temaapol          #+#    #+#             */
-/*   Updated: 2020/07/17 08:40:53 by temaapol         ###   ########.fr       */
+/*   Updated: 2020/07/17 14:18:38 by temaapol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -16,11 +16,11 @@ int ft_iterative_power(int nb, int power)
    int res;
 
    res = 1;
-   if((nb == 0 && power != 0)
+   if (nb == 0 || power < 0)
      {
 	   return (0);
 	 }
-   if((power == 0) || ((nb < 0) && (power == 0)))
+   if (power == 0)
      {
         return (1);
 	 }
